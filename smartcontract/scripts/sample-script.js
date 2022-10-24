f207b47e8dd3618788eb6a5a5733fde5f04d1c1d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-24 08:58:32 +0100

    install packages

--- smartcontract/scripts/sample-script.js
+++ smartcontract/scripts/sample-script.js
@@ -3,9 +3,11 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
 
 async function main() {
+
     // Hardhat always runs the compile task when running scripts with its command
     // line interface.
     //
@@ -14,6 +16,7 @@ async function main() {
     // await hre.run('compile');
 
     // We get the contract to deploy
+
     const Greeter = await hre.ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, Hardhat!");
 
@@ -24,6 +27,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {

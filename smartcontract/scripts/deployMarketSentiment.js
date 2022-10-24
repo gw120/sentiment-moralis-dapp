e3fe471cf9fc2397d6fb22f6ccfe47f37f72226e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-25 00:14:32 +0100

    add mumbai nretwork and deployment script - clean, compile, run, verify

---
+++ smartcontract/scripts/deployMarketSentiment.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const MarketSentiment = await hre.ethers.getContractFactory("MarketSentiment");
+    const marketsentiment = await MarketSentiment.deploy();
+
+    await marketsentiment.deployed();
+
+    console.log("MarketSentiment deployed to:", marketsentiment.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });

21b21c0ef2e50362f98994b529dd72de37f217cc
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-25 00:21:12 +0100

    add style parameters for liquid wave animation

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -11,7 +11,11 @@ function Coin({ perc, setPerc, token }) {
                 </div>
                 <div className="circle">
                     <div className="wave"
-
+                        style={{
+                            marginTop: `${100 - perc}%`,
+                            boxShadow: `0 0 20px green`,
+                            backgroundColor: "green",
+                        }}
                     >
                     </div>
                     <div className="percentage">

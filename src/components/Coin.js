76eb9bce0aad1cd9ef2de28848880bc933cb0fc5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-27 15:54:00 +0100

    call vote function with params on blockchain 💯

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+﻿import React, { useEffect, useState } from "react";
 import "./Coin.css";
 import { Button } from "web3uikit";
 
@@ -6,9 +6,16 @@ import LOGO_BTC from "../images/btc.png"
 import LOGO_ETH from "../images/eth.png"
 import LOGO_MATIC from "../images/poly.png"
 
+import { useWeb3ExecuteFunction, useMoralis } from "react-moralis";
+
 function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
     const [color, setColor] = useState();
     const [logoSymbol, setLogoSymbol] = useState()
+
+    const contractProcessor = useWeb3ExecuteFunction();
+    const { isAuthenticated } = useMoralis();
+
+
 
     useEffect(() => {
         if (perc < 50) {
@@ -30,6 +37,49 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
         }
     }, []);
 
+    async function vote(upDown) {
+
+        let options = {
+            contractAddress: "0x9A81aDc9dDF0c0b398d98A6A89Eb0Db3774A8423",
+            functionName: "vote",
+            abi: [
+                {
+                    "inputs": [
+                        {
+                            "internalType": "string",
+                            "name": "_ticker",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "bool",
+                            "name": "_vote",
+                            "type": "bool"
+                        }
+                    ],
+                    "name": "vote",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                }
+            ],
+            params: {
+                _ticker: token,
+                _vote: upDown,
+            },
+        }
+
+        await contractProcessor.fetch({
+            params: options,
+            onSuccess: () => {
+                console.log("vote successful", '✔');
+            },
+            onError: (error) => {
+                alert(error.data.message)
+            }
+        });
+
+
+    }
     return (
         <>
             <div>
@@ -56,8 +106,11 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
                 <div className="votes">
                     <Button
                         onClick={() => {
-                         setPerc(perc + 1)
-                        }}
+                            if (isAuthenticated) {
+                                vote(true)
+                            } else {
+                                alert("Authenicate to Vote")
+                            }                        }}
                         text="Up"
                         theme="primary"
                         type="button"
@@ -66,8 +119,11 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
                     <Button
                         color="red"
                         onClick={() => {
-                        setPerc(perc + 1)
-                        }}
+                            if (isAuthenticated) {
+                                vote(false)
+                            } else {
+                                alert("Authenicate to Vote")
+                            }                        }}
                         text="Down"
                         theme="colored"
                         type="button"
